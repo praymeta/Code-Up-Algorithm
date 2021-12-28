d7d9ab9f693754c6e1e81f4cbe688906839a5d8b
DESKTOP-K79VA5A\lhj99 <hyeokjun_0329@naver.com>
2021-12-28 23:53:58 +0900

    프로그래머스 레벨1 : 폰켓몬
    solution 함수 구현

---
+++ PRG1_Ponkemon.py
@@ -0,0 +1,15 @@
+def solution(nums):
+    answer = 0
+    myPokemon = []
+    i = 0
+    
+    while len(myPokemon) != len(nums)/2:
+        if myPokemon.count(nums[i]) == 0:
+            myPokemon.append(nums[i])
+        if i == len(nums)-1:
+            break
+        i += 1
+            
+    answer = len(myPokemon)
+        
+    return answer
