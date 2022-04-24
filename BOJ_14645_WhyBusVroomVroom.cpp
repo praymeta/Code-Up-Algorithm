fb260a2fc0e9634d89785cc5d9268a0b7c2445de
이혁준 <hyeokjun_0329@naver.com>
2022-04-24 13:16:36 +0900

    백준 14645 : 와이버스 부릉부릉
    버스 운전수 비와이 씨가 운전하는 버스는 N개의 정거장을 거친 후 종착역에 도착한다. 각 정거장은 내릴 인원수와 올라탈 인원수가 정해져 있다. 종착역에 도착하면 버스에 타고 있던 모든 사람이 내린다.
    종착역에 도착했을 때, 버스 운전수의 이름을 출력해라.

---
+++ BOJ_14645_WhyBusVroomVroom.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n, k, a, b;
+    cin >> n >> k;
+    for(int i = 0; i < n; i++) {
+        cin >> a >> b;
+    }
+    cout << "비와이" << endl;
+    return 0;
+}
