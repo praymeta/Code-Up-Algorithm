524c8bb911a21571946b6f60249e1174fcf19e18
이혁준 <hyeokjun_0329@naver.com>
2022-06-10 21:04:08 +0900

    백준 24568 : Cupcake Party
    A regular box of cupcakes holds 8 cupcakes, while a small box holds 3 cupcakes. There are 28 students in a class and a total of at least 28 cupcakes. Your job is to determine how many cupcakes will be left over if each student gets one cupcake.

---
+++ BOJ_24568_CupcakeParty.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int r, s, sum;
+    cin >> r;
+    cin >> s;
+    sum = 8 * r + 3 * s;
+    cout << sum - 28 << endl;
+
+    return 0;
+}
