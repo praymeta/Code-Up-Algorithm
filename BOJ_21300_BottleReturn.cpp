68236f1af88deaf7fd86a0fc438f8ac67e0e7553
이혁준 <hyeokjun_0329@naver.com>
2022-06-10 20:25:32 +0900

    백준 21300 : Bottle Return
    In the United States, beverage container deposit laws, or so-called bottle bills, are designed to reduce litter and reclaim bottles, cans and other containers for recycling. Ten states currently have some sort of deposit-refund systems in place for different types of beverage containers. These deposit amounts vary from 2¢ to 15¢ per container, depending on the type and volume of the container. For example, Oregon charges a (refundable) deposit of 2¢ per refillable container, and 10¢ for all others (with exceptions).
    
    For this problem you will calculate the amount a customer will get refunded for a given collection of empty containers in the state of New York. New York’s rules are very simple: there is a 5¢ deposit for containers of any size less than one gallon containing beer, malt, wine products, carbonated soft drinks, seltzer and water (that does not contain sugar).

---
+++ BOJ_21300_BottleReturn.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n, n1, n2, n3, n4, n5, n6;
+    cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6;
+    n = n1 + n2 + n3 + n4 + n5 + n6;
+    cout << n * 5 << endl;
+
+    return 0;
+}
