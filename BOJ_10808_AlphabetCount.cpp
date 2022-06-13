f26e1c597895f1f9967ed66bf97764907bdc282e
이혁준 <hyeokjun_0329@naver.com>
2022-06-13 21:52:18 +0900

    백준 10808 : 알파벳 개수 세기
    변경사항 저장 누락 수정

--- BOJ_10808_AlphabetCount.cpp
+++ BOJ_10808_AlphabetCount.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main(void) {
-    char str[100];
+    char str[101];
     int alphabet[26] = {0};
-    cin.getline(str, 100);
+    cin.getline(str, 101);
     for(int i = 0; i < strlen(str); i++) {
         alphabet[str[i] - 97]++;
     }
