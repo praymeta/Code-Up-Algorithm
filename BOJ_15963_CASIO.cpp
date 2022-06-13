75141076241fed809e09b73f2da227c20a9ed8b7
이혁준 <hyeokjun_0329@naver.com>
2022-06-14 04:43:00 +0900

    백준 15963 : CASIO
    카시오 계산기는 만능 계산기이다. 시험을 한 번이라도 쳐본 일곽인이라면, 이 카시오의 소중함에 대해서 뼈저리게 느껴보았을 것이다. 하지만, 이런 카시오에도 함정이 있다. 바로, 카시오 계산기는 배터리를 통해 돌아간다는 것이다.
    
    송찬이는 시험을 치다가 갑자기 계산기의 배터리가 나가버렸다. 그래서 선생님께 배터리를 달라고 요구했는데, 요구하고 보니 카시오 계산기의 배터리가 어떤 종류인지 말을 안 해버렸다! 과연 선생님은 송찬이가 필요한 배터리 종류를 들고 왔을까?

---
+++ BOJ_15963_CASIO.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    long n, m;
+    cin >> n >> m;
+    if (n == m) {
+        cout << "1" << endl;
+    } else {
+        cout << "0" << endl;
+    }
+    return 0;
+}
