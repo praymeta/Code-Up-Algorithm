28f08f825ad0156e6d2a23f75406dc89d56156ed
이혁준 <hyeokjun_0329@naver.com>
2022-07-01 17:59:31 +0900

    백준 7891 : Can you add this?
    Given two integers, calculate and output their sum.

---
+++ BOJ_7891_CanYouAddThis.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    long long x, y;
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        cin >> x >> y;
+        cout << x + y << endl;
+    }
+    return 0;
+}
