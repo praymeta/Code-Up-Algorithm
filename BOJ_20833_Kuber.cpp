b3352880f37240d3322983869b91867650f0b9b3
이혁준 <hyeokjun_0329@naver.com>
2022-08-18 19:31:20 +0900

    백준 20833 : 정육면체(Kuber)
    나드야는 한 변의 길이가 1인 작은 나무 정육면체를 풀로 붙여 더 큰 소형 나무 정육면체를 만듭니다. 나드야는 한 변의 길이가 각각 1부터 N인 정육면체를 만들기로 결정했습니다. 나드야에게 작은 정육면체가 몇 개 필요합니까?

---
+++ BOJ_20833_Kuber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n, sum = 0;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        sum += i * i * i;
+    }
+    cout << sum;
+    return 0;
+}
