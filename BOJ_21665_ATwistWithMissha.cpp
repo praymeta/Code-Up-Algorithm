4c56478d06af3878fc078e64081bfe5181d93443
이혁준 <hyeokjun_0329@naver.com>
2022-09-05 23:02:17 +0900

    백준 21665 : 미샤와 반전(Миша и негатив)
    입력 수정

--- BOJ_21665_ATwistWithMissha.cpp
+++ BOJ_21665_ATwistWithMissha.cpp
@@ -7,12 +7,12 @@ int main(void) {
     char bStr[n][m], aStr[n][m];
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            scanf("%1s", &bStr[i][j]);
+            cin >> bStr[i][j];
         }
     }
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            scanf("%1s", &aStr[i][j]);
+            cin >> aStr[i][j];
         }
     }
     for(int i = 0; i < n; i++) {
