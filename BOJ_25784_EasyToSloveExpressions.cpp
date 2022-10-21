be5db0e6b32a1db6a6fab6a7440bbbd788701ba5
이혁준 <hyeokjun_0329@naver.com>
2022-10-21 18:00:34 +0900

    백준 25784 : Easy-to-Solve Expressions
    When one looks at a set of numbers, one usually wonders if there is a relationship among them? This task is more manageable if there are only three numbers.
    
    Given three distinct positive integers, you are to determine how one can be computed using the other two. Print 1 if any of the three numbers is the sum of the other two numbers, print 2 if any of the three numbers is the product of the other two numbers, print 3 otherwise. Assume that exactly one of these three messages will apply.

---
+++ BOJ_25784_EasyToSloveExpressions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(a + b == c || b + c == a || a + c == b) {
+        cout << '1';
+    } else if(a * b == c || b * c == a || a * c == b) {
+        cout << '2';
+    } else {
+        cout << '3';
+    }
+}
