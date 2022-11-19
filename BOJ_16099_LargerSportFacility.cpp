df57e5d2239776abd2ed65d5ee7a4e3d2243cc4d
이혁준 <hyeokjun_0329@naver.com>
2022-11-19 18:42:36 +0900

    백준 16099 : Larger Sport Facility
    In a lot of places in the world, elite universities come in pairs and their students like to challenge each other every year. In England, Oxford and Cambridge are famous for The Boat Race, an annual rowing race that opposes them. In Switzerland, students from Zürich and Lausanne battle in a famous annual ski challenge.
    
    TelecomParisTech and Eurecom, two famous French schools, are also planning to organize a multi-sport tournament. They have already agreed on the choice of sports and the rules of the game, but the only point of disagreement is on where the contest should be hosted. Indeed, every school has been bragging for years about the wonderful sport facilities that they have.
    
    At last, it was agreed that the competition would be hosted at the school which has the larger rectangular sports field. The only thing left is to determine which school this is: given the size of the fields, determine which school has the field with the larger surface.

---
+++ BOJ_16099_LargerSportFacility.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n;
+    long long lt, wt, le, we, t, e;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> lt >> wt >> le >> we;
+        if (lt * wt > le * we) {
+            cout << "TelecomParisTech" << '\n';
+        } else if (le * we > lt * wt) {
+            cout << "Eurecom" << '\n';
+        } else {
+            cout << "Tie" << '\n';
+        }
+    }
+}
