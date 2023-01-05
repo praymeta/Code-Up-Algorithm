d3b2e5f0ba4c8d7539395c06921dcd8e568240db
이혁준 <hyeokjun_0329@naver.com>
2023-01-05 15:21:10 +0900

    백준 6841 : I Speak TXTMSG
    Text messaging using a cell phone is popular among teenagers. The messages can appear peculiar because short forms and symbols are used to abbreviate messages and hence reduce typing.
    
    For example, "LOL" means "laughing out loud" and ":-)" is called an emoticon which looks like a happy face (on its side) and it indicates chuckling. This is all quite a mystery to some adults.
    
    Write a program that will continually input a short form and output the translation for an adult using the following translation table:
    
    Short Form	Translation
    CU			see you
    :-)			I'm happy
    :-(			I'm unhappy
    ;-)			wink
    :-P			stick out my tongue
    (~.~)			sleepy
    TA			totally awesome
    CCC			Canadian Computing Competition
    CUZ			because
    TY			thank-you
    YW			you're welcome
    TTYL		talk to you later

---
+++ BOJ_6841_ISpeakTXTMSG.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    string str;
+    while(true) {
+        getline(cin, str);
+        if(str == "") break;
+        if(str == "CU") cout << "see you" << '\n';
+        else if(str == ":-)") cout << "I’m happy" << '\n';
+        else if(str == ":-(") cout << "I’m unhappy" << '\n';
+        else if(str == ";-)") cout << "wink" << '\n';
+        else if(str == ":-P") cout << "stick out my tongue" << '\n';
+        else if(str == "(~.~)") cout << "sleepy" << '\n';
+        else if(str == "TA") cout << "totally awesome" << '\n';
+        else if(str == "CCC") cout << "Canadian Computing Competition" << '\n';
+        else if(str == "CUZ") cout << "because" << '\n';
+        else if(str == "TY") cout << "thank-you" << '\n';
+        else if(str == "YW") cout << "you’re welcome" << '\n';
+        else if(str == "TTYL") cout << "talk to you later" << '\n';
+        else cout << str << '\n';
+    }
+}
