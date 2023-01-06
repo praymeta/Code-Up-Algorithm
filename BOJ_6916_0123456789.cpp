ac9dcbcde8e250c2433979ed63232c54f786ad56
이혁준 <hyeokjun_0329@naver.com>
2023-01-06 16:53:01 +0900

    백준 6916 : 0123456789
    Most digital devices show numbers using a seven segment display. The seven segments are arranged as shown:
    
     * * *
    *     *
    *     *
    *     *
     * * *
    *     *
    *     *
    *     *
     * * *
    
    For this problem, each segment is represented by three asterisks in a line as shown above.
    
    Any digit from 0 - 9 can be shown by illuminating the appropriate segments. For example, the digit 1 may be displayed using the two segments on the right side:
    
          *
          *
          *
    
          *
          *
          *
    
    The digit 4 needs four segments to display it properly:
    
    *     *
    *     *
    *     *
     * * *
          *
          *
          *
    
    Write a program that will accept a single digit input from the user, and then display that digit using a seven segment display. You may assume that each segment is composed of three asterisks.
    
    BOJ-specific note: None of your lines should contain any trailing whitespace. The last line must end with a newline.

---
+++ BOJ_6916_0123456789.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    if(n == 0) {
+        cout << " * * *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << " * * *" << '\n';
+    } else if(n == 1) {
+        cout << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << '\n';
+    } else if(n == 2) {
+        cout << " * * *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << " * * *" << '\n';
+        cout << "*" << '\n';
+        cout << "*" << '\n';
+        cout << "*" << '\n';
+        cout << " * * *" << '\n';
+    } else if(n == 3) {
+        cout << " * * *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << " * * *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << " * * *" << '\n';
+    } else if(n == 4) {
+        cout << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << " * * *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << '\n';
+    } else if(n == 5) {
+        cout << " * * *" << '\n';
+        cout << "*" << '\n';
+        cout << "*" << '\n';
+        cout << "*" << '\n';
+        cout << " * * *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << " * * *" << '\n';
+    } else if(n == 6) {
+        cout << " * * *" << '\n';
+        cout << "*" << '\n';
+        cout << "*" << '\n';
+        cout << "*" << '\n';
+        cout << " * * *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << " * * *" << '\n';
+    } else if(n == 7) {
+        cout << " * * *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << '\n';
+    } else if(n == 8) {
+        cout << " * * *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << " * * *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << " * * *" << '\n';
+    } else {
+        cout << " * * *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << "*     *" << '\n';
+        cout << " * * *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << "      *" << '\n';
+        cout << " * * *" << '\n';
+    }
+}
