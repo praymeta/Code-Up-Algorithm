38b1609bcd60665fca6da348716e1dae1833351c
이혁준 <hyeokjun_0329@naver.com>
2023-04-21 21:01:38 +0900

    프로그래머스 레벨 0: 홀짝 구분하기
    자연수 n이 입력으로 주어졌을 때, 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.

---
+++ PRG0_SeparateSips.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n;
+    cin >> n;
+    cout << n;
+    if(n % 2) cout << " is odd";
+    else cout << " is even";
+    return 0;
+}
