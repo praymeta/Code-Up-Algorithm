d9b38e3f38607c27d6e535d6cbcf4027b5012205
이혁준 <hyeokjun_0329@naver.com>
2023-04-22 23:55:49 +0900

    프로그래머스 입문: 두 수의 나눗셈
    정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 solution 함수를 완성해주세요.

---
+++ PRGIntro_DivisionOfTwoNumbers.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    double a = num1, b = num2;
+    answer = a / b * 1000;
+    return answer;
+}
