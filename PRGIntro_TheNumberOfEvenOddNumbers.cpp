068371b8293021e55729faf3a495148167c5fd01
이혁준 <hyeokjun_0329@naver.com>
2023-04-25 02:26:17 +0900

    프로그래머스 입문: 짝수 홀수 개수
    정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.

---
+++ PRGIntro_TheNumberOfEvenOddNumbers.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer(2);
+    for(int i = 0; i < num_list.size(); i++) {
+        if(num_list[i] % 2) answer[1]++;
+        else answer[0]++;
+    }
+    return answer;
+}
