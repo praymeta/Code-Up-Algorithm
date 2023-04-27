63c13a058a0af84d1e358c626379763c47e1cef7
이혁준 <hyeokjun_0329@naver.com>
2023-04-27 17:05:23 +0900

    프로그래머스 레벨 0: 배열 원소의 길이
    문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 return 하도록 solution 함수를 완성해주세요.

---
+++ PRG0_LengthOfArrayElement.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+    vector<int> answer(strlist.size());
+    for(int i = 0; i < strlist.size(); i++) {
+        answer[i] = strlist[i].length();
+    }
+    return answer;
+}
