17c6c88cc76336f2e70ff6ca78419023c273c070
이혁준 <hyeokjun_0329@naver.com>
2023-05-14 23:27:39 +0900

    백준 23530: Not A + B
    You are required to print a value or c which is not equal to the sum of two given numbers a and b.

---
+++ BOJ_23530_NotA+B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t, a, b;
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        cin >> a >> b;
+        cout << "1\n";
+    }
+}
