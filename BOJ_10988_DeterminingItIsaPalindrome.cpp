86c2737691e854b9ab628c3fddd9c9ba95bd1fcc
이혁준 <hyeokjun_0329@naver.com>
2023-05-18 15:18:12 +0900

    백준 10988: 팰린드롬인지 확인하기
    알파벳 소문자로만 이루어진 단어가 주어진다. 이때, 이 단어가 팰린드롬인지 아닌지 확인하는 프로그램을 작성하시오.
    
    팰린드롬이란 앞으로 읽을 때와 거꾸로 읽을 때 똑같은 단어를 말한다.
    
    level, noon을 팰린드롬이고, baekjoon, online, judge는 팰린드롬이 아니다.

---
+++ BOJ_10988_DeterminingItIsaPalindrome.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for(int i = 0; i < str.length() / 2; i++) {
+        if(str[i] != str[str.length() - 1 - i]) {
+            cout << 0;
+            return 0;
+        }
+    }
+    cout << 1;
+}
