934f2277ba1f6d79dca8f0157261ba4ccf55d156
praymeta <91721046+praymeta@users.noreply.github.com>
2023-07-24 23:53:13 +0900

    백준 2321: Crowing
    Farming seems to be the easy life. You have lots of time to practice your ascii art. Use your computer skills to produce the following ascii roster.

---
+++ BOJ_2321_Crowing.f
@@ -0,0 +1,8 @@
+program Crowing
+  print '(a)', ",~"
+  print '(a)', "( 9> //)"
+  print '(a)', " )(_///"
+  print '(a)', "( \_>/"
+  print '(a)', " \__/"
+  print '(a)', "  ^^"
+end program Crowing
