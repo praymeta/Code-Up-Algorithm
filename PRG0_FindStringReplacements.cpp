715a7c6d25aaa0d8b9a00f3c20ce7fe465212458
praymeta <91721046+praymeta@users.noreply.github.com>
2023-10-19 23:31:21 +0900

    프로그래머스 레벨 0: 문자열 바꿔서 찾기
    문자 "A"와 "B"로 이루어진 문자열 myString과 pat가 주어집니다. myString의 "A"를 "B"로, "B"를 "A"로 바꾼 문자열의 연속하는 부분 문자열 중 pat이 있으면 1을 아니면 0을 return 하는 solution 함수를 완성하세요.

---
+++ PRG0_FindStringReplacements.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+    int answer = 0;
+    for(int i = 0; i < myString.length(); i++) {
+        if(myString[i] == 'A')
+            myString[i] = 'B';
+        else
+            myString[i] = 'A';
+    }
+    if(myString.find(pat) != string::npos)
+        answer = 1;
+    return answer;
+}
