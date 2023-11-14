5927a9e9b350756c4245a320eb3236229979d63a
이혁준 <hyeokjun_0329@naver.com>
2023-11-14 23:29:19 +0900

    백준 1343: 폴리오미노
    민식이는 다음과 같은 폴리오미노 2개를 무한개만큼 가지고 있다. AAAA와 BB
    
    이제 '.'와 'X'로 이루어진 보드판이 주어졌을 때, 민식이는 겹침없이 'X'를 모두 폴리오미노로 덮으려고 한다. 이때, '.'는 폴리오미노로 덮으면 안 된다.
    
    폴리오미노로 모두 덮은 보드판을 출력하는 프로그램을 작성하시오.

---
+++ BOJ_1343_Polyomino.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main(void) {
+    string board, ans;
+    int cnt = 0;
+    cin >> board;
+    board += ' ';
+    for(int i = 0; i < board.size() - 1; i++) {
+        if(board[i] == 'X')
+            cnt++;
+        if(board[i] == '.') {
+            ans += ".";
+            if(cnt % 2 != 0)
+                break;
+            else
+                cnt = 0;
+        }
+        if(cnt == 2 && board[i + 1] != 'X') {
+            ans += "BB";
+            cnt = 0;
+        } else if(cnt == 4) {
+            ans += "AAAA";
+            cnt = 0;
+        }
+    }
+    if(cnt % 2 == 1)
+        cout << -1;
+    else
+        cout << ans;
+}
