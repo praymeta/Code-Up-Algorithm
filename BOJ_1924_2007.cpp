b37ac70ccbee33a6be32a9095bed3870120b98b9
praymeta <91721046+praymeta@users.noreply.github.com>
2023-11-22 23:55:51 +0900

    백준 1924: 2007년
    오늘은 2007년 1월 1일 월요일이다. 그렇다면 2007년 x월 y일은 무슨 요일일까? 이를 알아내는 프로그램을 작성하시오.

---
+++ BOJ_1924_2007.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+int main(void) {
+    int year[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+    string day[7] = { "SUN","MON","TUE","WED","THU","FRI","SAT" };
+    int x, y, sum = 0;
+    cin >> x >> y;
+    for (int i = 0; i < x-1; i++) {
+        sum += year[i];
+    }
+    sum += y;
+    cout << day[sum % 7];
+}
