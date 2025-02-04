1507d13f1aabba26e67a52edc5a38a45be21ee83
praymeta <hyeokjun_0329@naver.com>
2025-02-04 13:28:06 +0900

    백준 31654: Adding Trouble
    Your friend Bob is really bad at adding numbers, and he'd like some help to make sure he's doing it correctly! Can you help Bob make sure he is adding correctly? Given 3 integers A, B, C, make sure that A + B = C, and that Bob indeed added A and B correctly.

---
+++ BOJ_31654_AddingTrouble.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    if (A + B == C) {
+        cout << "correct!";
+    }
+    else {
+        cout << "wrong!";
+    }
+}
